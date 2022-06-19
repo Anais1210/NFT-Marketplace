f97a8d5772ba5f13aead7cdf88a82ee120f9a322
Anais1210 <azhang2@myges.fr>
2022-06-19 21:57:47 +0200

    update logo

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,8 @@ require("@nomiclabs/hardhat-ethers");
 const fs = require("fs");
 // const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
 require("dotenv").config();
+const projectId = "f32aecd12dd443bf824b32c0f58e86cf";
+
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -17,6 +19,14 @@ module.exports = {
   networks: {
     hardhat: {
       chainId: 1337,
+    },
+    mumbai: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: process.env.privateKey,
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: process.env.privateKey,
     },
     goerli: {
       url: process.env.REACT_APP_ALCHEMY_API_URL,
